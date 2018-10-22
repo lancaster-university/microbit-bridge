9cb4be5a6ec166c14849243c80c56882d380f3fe
James Devine <j.devine@lancaster.ac.uk>
2018-10-22 22:41:20 +0100

    Neatened up injection

--- inject.py
+++ inject.py
@@ -1,41 +1,70 @@
 from subprocess import call
-import re
+import re, sys
+from optparse import OptionParser
 
-call(["python","uf2conv.py","./hub-not-combined.hex", "-o", "./hub-not-combined.uf2"])
+SCHOOL_ID = "ABCDEFGHIJKL"
+HUB_ID = "ABCDEFGHIJKL"
 
-SCHOOL_ID = "THIS_IS_THE_UNIQUE_SCHOOL_ID"
-NEW_SCHOOL_ID = "ABCD_12_456_POOQUE_SCHOOL_ID"
-HUB_ID = "THIS_IS_THE_UNIQUE_HUB_ID"
-NEW_HUB_ID = "THIS_IS_THE_UNIQUE_HUB_ID"
+parser = OptionParser()
 
-print "Replacing previous school_id: %s with: %s" % (SCHOOL_ID, NEW_SCHOOL_ID)
-call(["sed","-i","s/"+SCHOOL_ID+"/"+NEW_SCHOOL_ID+"/", "./hub-not-combined.uf2"])
+#command line options
+parser.add_option("", "--school-id",
+                  action="store",
+	              type="string",
+                  dest="school_id",
+                  default="",
+                  help="The new school id to splice into the hub hex file")
 
-print "Replacing previous hub_id: %s with: %s" % (HUB_ID, NEW_HUB_ID)
-call(["sed","-i","s/"+HUB_ID+"/"+NEW_HUB_ID+"/", "./hub-not-combined.uf2"])
+parser.add_option("", "--hub-id",
+                  action="store",
+	              type="string",
+                  dest="hub_id",
+                  default="",
+                  help="The new hub id to splice into the hub hex file")
 
-# with open("./hub.uf2", "r+") as f:
-#     uf2 = f.readlines()
+(options, args) = parser.parse_args()
 
-#     new_lines = []
-#     for l in uf2:
-#         l = re.sub(SCHOOL_ID, "\1"+NEW_SCHOOL_ID, l)
-#         new_lines += [l]
-#     # uf2 = re.sub(SCHOOL_ID, "\1"+NEW_SCHOOL_ID, uf2)
-#     # uf2 = re.sub(HUB_ID, "\1"+NEW_HUB_ID, uf2)
+def inject_ids(NEW_SCHOOL_ID, NEW_HUB_ID):
 
-#     with open("./hub-test.uf2", "w") as f2:
-#         f2.writelines(new_lines)
+    if len(NEW_SCHOOL_ID) != len(SCHOOL_ID):
+        print "New school id length must much the old: size %d" % len(SCHOOL_ID)
+        return -1
 
-print "Converting back from uf2..."
-call(["python","uf2conv.py","./hub-not-combined.uf2", "-o", "./hub-not-combined-modified.bin"])
+    if len(NEW_HUB_ID) != len(HUB_ID):
+        print "New hub id length must much the old: size %d" % len(HUB_ID)
+        return -1
 
-print "Creating final hub.hex..."
-call(["python","bin2hex.py", "--offset=0x18000", "./hub-not-combined-modified.bin", "./hub-not-combined-modified.hex"])
-call(["python","merge_hex.py","./BOOTLOADER.hex", "./SOFTDEVICE.hex", "./hub-not-combined-modified.hex", "-o./hub.hex"])
+    # first convert the uncombined hex file into uf2
+    call(["python","uf2conv.py","./hub-not-combined.hex", "-o", "./hub-not-combined.uf2"])
 
-print "Cleaning up..."
-call(["rm", "./hub-not-combined-modified.hex", "./hub-not-combined-modified.bin", "./hub-not-combined.uf2"])
+    # replace the old ids with the new.
+    with open("./hub-not-combined.uf2", "r+") as f:
+        uf2 = f.readlines()
+        f.seek(0)
+
+        for l in uf2:
+            new_l = re.sub(SCHOOL_ID, NEW_SCHOOL_ID, l)
+            new_l = re.sub(HUB_ID, NEW_HUB_ID, new_l)
+            f.write(new_l)
+
+        f.close()
+
+    # convert to bin
+    print "Converting back from uf2..."
+    call(["python","uf2conv.py","./hub-not-combined.uf2", "-o", "./hub-not-combined-modified.bin"])
+
+    # then to hex
+    print "Creating final hub.hex..."
+    call(["python","bin2hex.py", "--offset=0x18000", "./hub-not-combined-modified.bin", "./hub-not-combined-modified.hex"])
+
+    # finally creating the final binary.
+    call(["python","merge_hex.py","./BOOTLOADER.hex", "./SOFTDEVICE.hex", "./hub-not-combined-modified.hex", "-o./hub.hex"])
+
+    print "Cleaning up..."
+    call(["rm", "./hub-not-combined-modified.hex", "./hub-not-combined-modified.bin", "./hub-not-combined.uf2"])
+
+if __name__ == '__main__':
+    sys.exit(inject_ids(options.school_id, options.hub_id))
 
 # call
 # call(["sed -i -E 's/(THIS_IS_THE_UNIQUE_SCHOOL_ID)/\1POOP/' ./hub.uf2"])
