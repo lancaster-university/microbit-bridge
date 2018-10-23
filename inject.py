74718e5adbfbad381be4bc4bb5d94a4595b27bbe
James Devine <j.devine@lancaster.ac.uk>
2018-10-23 14:27:49 +0100

    refactor for uniqueness of filenames
    Patch contributed by Adrian Dankiv

--- inject.py
+++ inject.py
@@ -1,6 +1,7 @@
 from subprocess import call
 import re, sys
 from optparse import OptionParser
+import tempfile
 
 SCHOOL_ID = "ABCDEFGHIJKL"
 HUB_ID = "ABCDEFGHIJKL"
@@ -10,64 +11,63 @@ parser = OptionParser()
 #command line options
 parser.add_option("", "--school-id",
                   action="store",
-	              type="string",
+                  type="string",
                   dest="school_id",
                   default="",
                   help="The new school id to splice into the hub hex file")
 
 parser.add_option("", "--hub-id",
                   action="store",
-	              type="string",
+                  type="string",
                   dest="hub_id",
                   default="",
                   help="The new hub id to splice into the hub hex file")
 
+parser.add_option("", "--output-file",
+                  action="store",
+                  type="string",
+                  dest="output_file_path",
+                  default="./hub.hex",
+                  help="Output file path")
+
 (options, args) = parser.parse_args()
 
-def inject_ids(NEW_SCHOOL_ID, NEW_HUB_ID):
+def inject_ids(new_school_id, new_hub_id, output_file_path):
 
-    if len(NEW_SCHOOL_ID) != len(SCHOOL_ID):
+    if len(new_school_id) != len(SCHOOL_ID):
         print "New school id length must much the old: size %d" % len(SCHOOL_ID)
         return -1
 
-    if len(NEW_HUB_ID) != len(HUB_ID):
+    if len(new_hub_id) != len(HUB_ID):
         print "New hub id length must much the old: size %d" % len(HUB_ID)
         return -1
 
-    # first convert the uncombined hex file into uf2
-    call(["python","uf2conv.py","./hub-not-combined.hex", "-o", "./hub-not-combined.uf2"])
+    with tempfile.NamedTemporaryFile() as hub_not_combined_modified_hex_file, \
+            tempfile.NamedTemporaryFile() as hub_not_combined_modified_bin_file, \
+            tempfile.NamedTemporaryFile() as hub_not_combined_uf2_file:
 
-    # replace the old ids with the new.
-    with open("./hub-not-combined.uf2", "r+") as f:
-        uf2 = f.readlines()
-        f.seek(0)
+        # first convert the uncombined hex file into uf2
+        call(["python","uf2conv.py","./hub-not-combined.hex", "-o", hub_not_combined_uf2_file.name])
+
+        # replace the old ids with the new.
+        uf2 = hub_not_combined_uf2_file.readlines()
+        hub_not_combined_uf2_file.seek(0)
 
         for l in uf2:
-            new_l = re.sub(SCHOOL_ID, NEW_SCHOOL_ID, l)
-            new_l = re.sub(HUB_ID, NEW_HUB_ID, new_l)
-            f.write(new_l)
+            new_l = re.sub(SCHOOL_ID, new_school_id, l)
+            new_l = re.sub(HUB_ID, new_hub_id, new_l)
+            hub_not_combined_uf2_file.write(new_l)
 
-        f.close()
+        # convert to bin
+        print "Converting back from uf2..."
+        call(["python","uf2conv.py",hub_not_combined_uf2_file.name, "-o", hub_not_combined_modified_bin_file.name])
 
-    # convert to bin
-    print "Converting back from uf2..."
-    call(["python","uf2conv.py","./hub-not-combined.uf2", "-o", "./hub-not-combined-modified.bin"])
+        # then to hex
+        print "Creating final file: %s" % (output_file_path)
+        call(["python","bin2hex.py", "--offset=0x18000", hub_not_combined_modified_bin_file.name, hub_not_combined_modified_hex_file.name])
 
-    # then to hex
-    print "Creating final hub.hex..."
-    call(["python","bin2hex.py", "--offset=0x18000", "./hub-not-combined-modified.bin", "./hub-not-combined-modified.hex"])
-
-    # finally creating the final binary.
-    call(["python","merge_hex.py","./BOOTLOADER.hex", "./SOFTDEVICE.hex", "./hub-not-combined-modified.hex", "-o./hub.hex"])
-
-    print "Cleaning up..."
-    call(["rm", "./hub-not-combined-modified.hex", "./hub-not-combined-modified.bin", "./hub-not-combined.uf2"])
+        # finally creating the final binary.
+        call(["python","merge_hex.py","./BOOTLOADER.hex", "./SOFTDEVICE.hex", hub_not_combined_modified_hex_file.name, "-o" + output_file_path])
 
 if __name__ == '__main__':
-    sys.exit(inject_ids(options.school_id, options.hub_id))
-
-# call
-# call(["sed -i -E 's/(THIS_IS_THE_UNIQUE_SCHOOL_ID)/\1POOP/' ./hub.uf2"])
-# call(["sed", "-i", "-E", "'s/THIS_IS_THE_UNIQUE_SCHOOL_ID/POOP/'","./hub.uf2"])
-
-# os.remove("./hub.uf2")
+    sys.exit(inject_ids(options.school_id, options.hub_id, options.output_file_path))
