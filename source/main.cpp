84892c837afa26f069a5ea51cdfaed436198efd4
James Devine <j.devine@lancaster.ac.uk>
2018-11-07 11:39:39 +0000

    add hub.hex for trial

--- source/main.cpp
+++ source/main.cpp
@@ -35,39 +35,98 @@ extern const char* SCHOOL_ID;
 extern const char* HUB_ID;
 
 PeridoBridge bridge(uBit.radio, uBit.serial, uBit.messageBus, uBit.display);
+// void process_packet(PeridoFrameBuffer*)
+// {
 
-void change_display()
-{
-    // disp++;
-    // uBit.display.print(disp);
-}
+// }
 
-void log_string_ch(const char* c)
-{
-    // uBit.serial.printf("%s ", c);
-}
+// void change_display()
+// {
+//     // disp++;
+//     // uBit.display.print(disp);
+// }
 
-void log_string(char c)
-{
-    // uBit.serial.printf("%c ", c);
-}
+// void log_string_ch(const char* c)
+// {
+//     // uBit.serial.printf("%s\r\n", c);
+// }
 
-void log_num(int c)
-{
-    // uBit.serial.printf("%d ", c);
-}
+// void log_string(const char* c)
+// {
+//     // uBit.serial.printf("%c ", c);
+// }
+
+// void log_ptr(uint8_t* ptr)
+// {
+//     // uBit.serial.printf("%p\r\n", ptr);
+// }
+
+// void log_num(int c)
+// {
+//     // uBit.serial.printf("%d\r\n", c);
+// }
+
+// void set_gpio0(int val)
+// {
+//     uBit.io.P0.setDigitalValue(val);
+// }
+
+// void set_gpio1(int val)
+// {
+//     uBit.io.P1.setDigitalValue(val);
+// }
+
+// void set_gpio2(int val)
+// {
+//     uBit.io.P2.setDigitalValue(val);
+// }
+
+// void set_gpio3(int val)
+// {
+//     uBit.io.P8.setDigitalValue(val);
+// }
+
+// void set_gpio4(int val)
+// {
+//     uBit.io.P12.setDigitalValue(val);
+// }
+
+// void set_gpio5(int val)
+// {
+//     uBit.io.P13.setDigitalValue(val);
+// }
+
+// void set_gpio6(int val)
+// {
+//     uBit.io.P14.setDigitalValue(val);
+// }
+
+// void set_gpio7(int val)
+// {
+//     uBit.io.P15.setDigitalValue(val);
+// }
+
+// void display_text(const char* text)
+// {
+//     uBit.display.scroll(text);
+// }
+
+// void display_number(int num)
+// {
+//     uBit.display.scroll(num);
+// }
 
 void blink()
 {
     int toggle = 0;
     while(1)
     {
-        uBit.display.image.setPixelValue(RUNNING_TOGGLE_PASTE_COLUMN, RUNNING_TOGGLE_PASTE_ROW, toggle);
+        uBit.display.image.setPixelValue(BLINK_POSITION_X, BLINK_POSITION_Y, toggle);
         toggle = !toggle;
-        uBit.sleep(400);
+        uBit.sleep(500);
     }
 }
-
+extern uint32_t radio_status;
 int main()
 {
     // Initialise the micro:bit runtime.
@@ -76,6 +135,15 @@ int main()
     uBit.radio.enable();
 
     create_fiber(blink);
+
+    // while(1)
+    // {
+    //     uBit.serial.printf("TXQ DEPTH: %d\r\n", uBit.radio.txQueueDepth);
+    //     uBit.serial.printf("RXQ DEPTH: %d\r\n", uBit.radio.rxQueueDepth);
+    //     uBit.serial.printf("RADIO_STATE: %d\r\n", NRF_RADIO->STATE);
+    //     uBit.serial.printf("RADIO_STATUS: %d\r\n", radio_status);
+    //     uBit.sleep(1000);
+    // }
 
     // uBit.display.print(disp);
     // uBit.display.scroll(SCHOOL_ID);
